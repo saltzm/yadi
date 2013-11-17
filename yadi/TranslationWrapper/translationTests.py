6303130b3ae0a3b279338354bf6ff7bf3864d089
nishara <nishara.pdn@gmail.com>
2013-11-17 19:00:36 +0100

    query evaluation

--- yadi/TranslationWrapper/translationTests.py
+++ yadi/TranslationWrapper/translationTests.py
@@ -9,31 +9,30 @@ __author__ = 'caioseguin'
 
 def main():
     datatog_statements = []
-    datalog_statements.append('q(X):-not(s(Y)),X=Y,Y=2.')
-    datalog_statements.append('q(X):- s(X), X = 2.')
-    datalog_statements.append('r(X,Y),!s(Z), Y=Z.')
-    datalog_statements.append('r(X,Y),!s(Z).')
-    datalog_statements.append('a(X,Y) :- r(X,A), s(A,Y).')
-    datalog_statements.append('a(X,2) :- r(X,A), s(A,_).')
-    datalog_statements.append('r(_,2).')
-    datalog_statements.append('r(_,A), s(A,_)..')
-    datalog_statements.append('r(X,Y) :- s(X), s(Y), X>Y.')
-    datalog_statements.append('r(X,Y) :- s(X), Y>2')
-    datalog_statements.append('r(X) :- s(X), X<2')
-    datalog_statements.append('a(X,Y):-s(X,Z),s(Y,Z),X>Y.')
-    datalog_statements.append('s(X) :- X = 2, 3<X.')
-    datalog_statements.append('r(_,2), X = 2.')
-    datalog_statements.append('a(X,Y) :- s(X), sa(Y,T), t(X), u(Y), X>Y.')
-    datalog_statements.append("q(X,Y):- s(X,Y). a(X,Y):- b(X,Y).")
+    datatog_statements.append('users(X,Y):- tuser(X,Y).')
+    #datatog_statements.append('q(X):-not(s(Y)),X=Y,Y=2.')
+    #datatog_statements.append('q(X):- s(X), X = 2.')
+    #datatog_statements.append('r(X,Y),!s(Z), Y=Z.')
+    #datatog_statements.append('r(X,Y),!s(Z).')
+    #datatog_statements.append('a(X,Y) :- r(X,A), s(A,Y).')
+    #datatog_statements.append('a(X,2) :- r(X,A), s(A,_).')
+    #datatog_statements.append('r(_,2).')
+    #datatog_statements.append('r(_,A), s(A,_)..')
+    #datatog_statements.append('r(X,Y) :- s(X), s(Y), X>Y.')
+    #datatog_statements.append('r(X,Y) :- s(X), Y>2')
+    #datatog_statements.append('r(X) :- s(X), X<2')
+    #datatog_statements.append('a(X,Y):-s(X,Z),s(Y,Z),X>Y.')
+    #datatog_statements.append('s(X) :- X = 2, 3<X.')
+    #datatog_statements.append('r(_,2), X = 2.')
+    #datatog_statements.append('a(X,Y) :- s(X), sa(Y,T), t(X), u(Y), X>Y.')
+    #datatog_statements.append("q(X,Y):- s(X,Y). a(X,Y):- b(X,Y).")
 
-    #datalog_statement = "q(X,Y):- s(X,Y). a(X,Y):- b(X,Y)."
-    datalog_statement = "m(X,Y):- tuser(X,Y)."
+    for d_query in datatog_statements:
+        #print(translateDatalogToSql(d_query))
+        sql_list = translateDatalogToSql(d_query)
 
-    output = translateDatalogToSql(datalog_statement)
-    print(output)
-    for sqlS in output:
+    for sqlS in sql_list:
         evaluateQuery().evaluate(sqlS)
-    for test in datalog_statements:
-        print(translateDatalogToSql(datalog_statement))
+
 
 main()
