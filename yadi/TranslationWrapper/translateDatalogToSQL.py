2bc70dd8dea2a3a37339e98dcbfe442cd704e5b9
Francisco Rodriguez Drumond <frandres@gmail.com>
2013-11-17 18:29:23 +0100

    gadfag

--- yadi/TranslationWrapper/translateDatalogToSQL.py
+++ yadi/TranslationWrapper/translateDatalogToSQL.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 from ASTFactory.ASTBuilder import ASTBuilder
 from parserAST.Parser import parser
 from queryExecutor.queryExecutor import QueryExecutor
--- yadi/TranslationWrapper/translationTests.py
+++ yadi/TranslationWrapper/translationTests.py
@@ -3,8 +3,25 @@ from TranslationWrapper.translateDatalogToSQL import translateDatalogToSql
 __author__ = 'caioseguin'
 
 def main():
+    datatog_statements = []
+    datalog_statements.append('q(X):-not(s(Y)),X=Y,Y=2.')
+    datalog_statements.append('q(X):- s(X), X = 2.')
+    datalog_statements.append('r(X,Y),!s(Z), Y=Z.')
+    datalog_statements.append('r(X,Y),!s(Z).')
+    datalog_statements.append('a(X,Y) :- r(X,A), s(A,Y).')
+    datalog_statements.append('a(X,2) :- r(X,A), s(A,_).')
+    datalog_statements.append('r(_,2).')
+    datalog_statements.append('r(_,A), s(A,_)..')
+    datalog_statements.append('r(X,Y) :- s(X), s(Y), X>Y.')
+    datalog_statements.append('r(X,Y) :- s(X), Y>2')
+    datalog_statements.append('r(X) :- s(X), X<2')
+    datalog_statements.append('a(X,Y):-s(X,Z),s(Y,Z),X>Y.')
+    datalog_statements.append('s(X) :- X = 2, 3<X.')
+    datalog_statements.append('r(_,2), X = 2.')
+    datalog_statements.append('a(X,Y) :- s(X), sa(Y,T), t(X), u(Y), X>Y.')
+    datalog_statements.append("q(X,Y):- s(X,Y). a(X,Y):- b(X,Y).")
 
-    datalog_statement = "q(X,Y):- s(X,Y). a(X,Y):- b(X,Y)."
-    print(translateDatalogToSql(datalog_statement))
+    for test in datalog_statements:
+        print(translateDatalogToSql(datalog_statement))
 
 main()
