21612400a6458324af42f09bfe429e64a1824ca0
Matthew Saltz <saltzm@gmail.com>
2013-11-17 01:12:36 +0100

    Delete importScript.py

--- importScript.py
+++
@@ -1,6 +0,0 @@
-from yadi.dataStructures.query import *
-from yadi.dataStructures.relation import *
-from yadi.dataStructures.element import *
-from yadi.dataStructures.constraint import *
-from yadi.queryExecutor.queryExecutor import *
-from yadi.queryExecutor.sqlFactory import *
