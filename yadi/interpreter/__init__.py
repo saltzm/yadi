0b3caeb05509dad721ba6f1ea11ae83ae490c13e
Jorge Samper Gonzalez <jsampergonzalez@gmail.com>
2013-11-17 23:16:56 +0100

    Interpreter

---
+++ yadi/interpreter/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jorge Samper Gonzalez'
+__email__ = 'jsampergonzalez@gmail.com'
+__version__ = '0.1.0'
+__all__ = ["interpreter"]
--- yadi/yadi.py
+++ yadi/yadi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import sys
-sys.path.append('/home/nishara/PycharmProjects/yadi/yadi')
+from interpreter.interpreter import *
+
+if __name__ == '__main__':
+    Interpreter().cmdloop()
