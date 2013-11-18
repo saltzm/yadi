1423598e342bae60245374d6ffadb1151c824c3e
Francisco Rodriguez Drumond <frandres@gmail.com>
2013-11-18 01:23:20 +0100

    Exceptions handled only in the wrapper

--- yadi/queryExecutor/queryExecutor.py
+++ yadi/queryExecutor/queryExecutor.py
@@ -4,11 +4,10 @@ from .sqlFactory import *
 
 class QueryExecutor():
     def execute_query(self, query):        
-        try:
-            new_query = QueryPreprocessor().preprocess(query)
-            SafetyChecker().check_for_safety(new_query)        
-        except SafetyException as e:
-            return str(e)
-        print ('Processing: \n' + str(new_query))
+        
+        new_query = QueryPreprocessor().preprocess(query)
+        SafetyChecker().check_for_safety(new_query)        
+
+        #print ('Processing: \n' + str(new_query))
         sql = SQLGenerator().get_SQL_code(new_query,query)
         return sql
