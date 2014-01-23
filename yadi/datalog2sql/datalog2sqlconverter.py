0cdadc8ee6e35804e8f5d63b39e2bfe37ce2e7d9
saltzm <saltzm@gmail.com>
2014-01-23 15:58:58 +0100

    reworked datalog2sqlconverter.py

--- yadi/datalog2sql/datalog2sqlconverter.py
+++ yadi/datalog2sql/datalog2sqlconverter.py
@@ -1,24 +1,21 @@
 from ..ast_factory.ASTBuilder import *
 from ..parser_ast.Parser import parser,SyntaxException
-from ..query_executor.queryExecutor import QueryExecutor
-from ..query_executor.exceptions import *
+from ..ast2sql.ast2sqlconverter import Ast2SqlConverter
+from ..ast2sql.exceptions import *
 from colorama import *
 
 __author__ = 'caioseguin'
 
 class Datalog2SqlConverter:
     def convertDatalogToSql(datalog_statement):
-        datalog_parser = parser()
-        ast_builder = ASTBuilder()
-        query_executor = QueryExecutor()
         sql_query_list = []
 
         try:
-            parsed_statement = datalog_parser.parsesentence(datalog_statement)
-            ast_query_list = ast_builder.buildAST(parsed_statement.asList())
+            parsed_statement = parser().parsesentence(datalog_statement)
+            ast_query_list = ASTBuilder().buildAST(parsed_statement.asList())
 
             for ast_query in ast_query_list:
-                sql_query = QueryExecutor().execute_query(ast_query)
+                sql_query = Ast2SqlConverter().convertAst2Sql(ast_query)
                 sql_query_list.append(sql_query)
         except SyntaxException as e:
             print (Fore.RED+'SyntaxException: ' + str(e)+Fore.RESET)
